5d10ac9cbe904dc90f578cf29d6166273f2c6a5d
timothehippo <tyxhul@yahoo.com>
2017-10-28 19:48:05 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,7 +7,7 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def '#input_to_index'(number)
+def input_to_index(number)
 
 finalnumber = number.to_i
 board[finalnumber]="x"
